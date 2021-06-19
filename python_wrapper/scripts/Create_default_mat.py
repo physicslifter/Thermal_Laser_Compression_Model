4b466db8523d43f1e0cfc9a7be091eb75324e4f7
Patrick Gavin LaChapelle <patlachapelle3@gmail.com>
2021-06-19 08:58:00 -0700

    adding the sample matrix, and the script for creating the default matrix

---
+++ python_wrapper/scripts/Create_default_mat.py
@@ -0,0 +1,60 @@
+#A file for writing the .mat input for the FEM in python.
+#The subsequent step to this in the process will be to send the input to the MATLAB file
+#to run in the python matlab engine
+from scipy import io
+
+#===============================
+#Writing the Default values
+#===============================
+
+#write the matrix variables (a list of the variables can be found in )
+#===============================================================================
+#Initial data refinement
+#===============================================================================
+sop_lineouts_data='s88773_sop_lineouts.xlsx' #.xlsx file with the sop lineout data
+reflectivity_data='Reflectivity_s88773.xlsx' #.xlsx file containing the reflectivity data to use
+throughput=1.0 #throughput correction based on slit width and shot number
+aeta= 25.5#1/delta_t of SOP data
+a0= 481000.0# based on ND filter
+t0a= 1.909# based on ND filter
+
+#===============================================================================
+#FEM
+#===============================================================================
+
+#sample
+density=12500.0 #density kg/m^3 previous at 11000
+heat_capacity=500.0 #heat capacity J/kg K
+t0=2000.0 #initial temp K/counts
+width=1.0 #width (um)
+
+#window
+km=100.0
+dm=4200.0
+cm=800.0
+t0m=2000.0
+
+#==================================================
+
+#Now write a dictionary for the default values
+default_values={
+    'sop_lineouts_data':sop_lineouts_data,
+    'reflectivity_data':reflectivity_data,
+    'xw':throughput,
+    'aeta':aeta,
+    'a0':a0,
+    't0a':t0a,
+    'd':density,
+    'c':heat_capacity,
+    't0':t0,
+    'w':width,
+    'km':km,
+    'dm':dm,
+    'cm':cm,
+    't0m':t0m
+}
+
+#Finally, save the dictionary as a .mat file
+io.savemat('../inputs/default_input_matrix.mat',mdict=default_values)
+
+
