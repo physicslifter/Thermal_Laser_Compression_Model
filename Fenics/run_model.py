8bddf088c6b7756d39ab38d18880cff2a0eeeb72
physicslifter <patlachapelle3@gmail.com>
2022-02-04 11:02:52 -0800

    added functionality for start_time parameter

--- Fenics/run_model.py
+++ Fenics/run_model.py
@@ -15,7 +15,7 @@ def convert_geometry(length, density=12800, ambient_density=7900):
     
     return new_length
 
-def main(run_IDs, num_steps, a, b, peak_temp):
+def main(run_IDs, num_steps, a, b, peak_temp, start_time):
     num_runs=len(run_IDs)
     geometry_dict=load_json('geometry_dict.json')
     values_dict=load_json('default_values_dict.json')
@@ -23,9 +23,9 @@ def main(run_IDs, num_steps, a, b, peak_temp):
     for x in range(num_runs):
         runID=run_IDs[x] # get the xth name of the run specified in run_IDs input
         solution={
-            'face1': run_simple_fem(tf=values_dict['tf'], num_steps=num_steps,a=a, b=b, peak_temp=peak_temp, init_temp=values_dict['init_temp'], k_1=values_dict['k_1'], rho=values_dict['rho'], c=values_dict['c'], Fe_length=convert_geometry(geometry_dict[runID]['face1']) , MgO_length=convert_geometry(values_dict['MgO_length'])),
-            'face2': run_simple_fem(tf=values_dict['tf'], num_steps=num_steps,a=a, b=b, peak_temp=peak_temp, init_temp=values_dict['init_temp'], k_1=values_dict['k_1'], rho=values_dict['rho'], c=values_dict['c'], Fe_length=convert_geometry(geometry_dict[runID]['face2']) , MgO_length=convert_geometry(values_dict['MgO_length'])),
-            'face3': run_simple_fem(tf=values_dict['tf'], num_steps=num_steps,a=a, b=b, peak_temp=peak_temp, init_temp=values_dict['init_temp'], k_1=values_dict['k_1'], rho=values_dict['rho'], c=values_dict['c'], Fe_length=convert_geometry(geometry_dict[runID]['face3']) , MgO_length=convert_geometry(values_dict['MgO_length']))
+            'face1': run_simple_fem(tf=values_dict['tf'], num_steps=num_steps,a=a, b=b, peak_temp=peak_temp, init_temp=values_dict['init_temp'], k_1=values_dict['k_1'], rho=values_dict['rho'], c=values_dict['c'], Fe_length=convert_geometry(geometry_dict[runID]['face1']) , MgO_length=convert_geometry(values_dict['MgO_length']), start_time=start_time),
+            'face2': run_simple_fem(tf=values_dict['tf'], num_steps=num_steps,a=a, b=b, peak_temp=peak_temp, init_temp=values_dict['init_temp'], k_1=values_dict['k_1'], rho=values_dict['rho'], c=values_dict['c'], Fe_length=convert_geometry(geometry_dict[runID]['face2']) , MgO_length=convert_geometry(values_dict['MgO_length']), start_time=start_time),
+            'face3': run_simple_fem(tf=values_dict['tf'], num_steps=num_steps,a=a, b=b, peak_temp=peak_temp, init_temp=values_dict['init_temp'], k_1=values_dict['k_1'], rho=values_dict['rho'], c=values_dict['c'], Fe_length=convert_geometry(geometry_dict[runID]['face3']) , MgO_length=convert_geometry(values_dict['MgO_length']), start_time=start_time)
         }
         results[runID]=solution
     return results
